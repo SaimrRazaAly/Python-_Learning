5d445d4baffcd6c1f0a84a1b7b5956d429fc7d1b
SaimRazaAly <saimrazateach2010@gmail.com>
2025-01-24 19:51:51 -0800

    learned about data types

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
 print("Welcome to Python!")
+
+# phyton dataTypes
+# 1. int (2)
+# 2. float (2.5)
+# 3. string ("Hello")
+# 4. boolean (True, False)
+# 5. list (["apple", "banana", "cherry"])
+# 6. tuple (("apple", "banana", "cherry"))
+# 7. dictionary ({"name" : "saim", "age" : 16})
+# 8. set ({"apple", "banana", "cherry"})
