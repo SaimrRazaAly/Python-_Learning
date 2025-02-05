3d682c361a3322ddd21440a414a9b5c23fa04c07
SaimRazaAly <saimrazateach2010@gmail.com>
2025-02-05 09:20:02 -0800

    another py program on function solved

--- Pratice/Functions_Practice.py
+++ Pratice/Functions_Practice.py
@@ -10,3 +10,11 @@ def Square(number):
 
 Squared_Number = Square(2)
 print(Squared_Number)
+
+
+# Q2 Write a program that gives sum of two numbers
+
+def Add_Two_Num(a,b):
+    return a+b
+print(Add_Two_Num(5,6)) # 11
+print(Add_Two_Num("5","6")) #56
