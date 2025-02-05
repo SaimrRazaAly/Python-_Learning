8e7ce079fccae7bf54fb8930c96e6465b18ab5be
SaimRazaAly <saimrazateach2010@gmail.com>
2025-02-05 09:37:55 -0800

    one more problem soveld

--- Pratice/Functions_Practice.py
+++ Pratice/Functions_Practice.py
@@ -18,3 +18,15 @@ def Add_Two_Num(a,b):
     return a+b
 print(Add_Two_Num(5,6)) # 11
 print(Add_Two_Num("5","6")) #56
+
+# Q3 Make a program that gives Circumfarece and area of cirlce when radius is given
+import math 
+
+def Circle_opeation(radius):
+    area = math.pi * (radius ** 2)
+    circumference = (2 * math.pi) * radius
+    return area ,circumference
+
+a,c = Circle_opeation(41)
+
+print("the area is,",round(a,1),"\nthe cercomferece is ,", round(c,1),)
